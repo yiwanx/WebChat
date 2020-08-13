f14555d6aa62dbca83b0d2f99e244867544d0a30
Ivan Petukhov <yiwanv@gmail.com>
2020-08-13 20:22:04 +0300

    Chat.jsx is implemented; server responds on join

--- front/src/components/Create.jsx
+++ front/src/components/Create.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react";
 import {Link} from "react-router-dom";
-
+import '../../App.css'
 const Create = () => {
     const [name, setName] = useState('')
     const [room, setRoom] = useState('')
@@ -8,6 +8,7 @@ const Create = () => {
     const handleRoomChange = event => setRoom(event.target.value)
     return (
         <div className="container">
+            <div className="form">
             <h1>Sign in (Create new Room)</h1>
             <div>
                 <input placeholder="Name"
@@ -18,6 +19,7 @@ const Create = () => {
             <Link onClick={event => (!room || !name) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
                 <button type="submit" text="Sign in">Sign In</button>
             </Link>
+            </div>
         </div>
     )
 }
